231808bfdde77554237f7b7b19240bf9b230d008
Zaham2 <zaham2@yahoo.com>
2023-11-29 09:13:47 +0200

    database setup

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,19 @@ const prisma = require('./lib/prisma')
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const cors = require('cors');
 
 // require the .env file
 require('dotenv').config();
 
+app.use(cors())
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.get('/api', (req, res) => {
-    
+app.get('/api', async (req, res) => {
+    const result = await prisma.rule.findMany()
+    console.log(result)
+    res.send(result)
 
 })
 
