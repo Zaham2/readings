7e00d7b29c104cb2a77a1bc56b90c1fbce0b1eba
Zaham2 <zaham2@yahoo.com>
2023-12-01 14:16:05 +0200

    Inregrated prisma database with backend

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,58 @@
 // create an express server that listens to a PORT environment variable
-const prisma = require('./lib/prisma')
-const express = require('express');
+import prisma from './lib/prisma.js';
+import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
-const cors = require('cors');
+import cors from 'cors';
 
 // require the .env file
-require('dotenv').config();
+// require('dotenv').config();
 
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', async (req, res) => res.send('Hello World!'));
 
 app.get('/api', async (req, res) => {
-    const result = await prisma.rule.findMany()
+    res.send(await prisma.rule.findMany())
+})
+
+app.get('/api/:id', async (req, res) => {
+    const result = await prisma.rule.findUnique({
+        where: { id: req.params?.id }
+    })
     console.log(result)
     res.send(result)
+})
 
+app.post('/api', async (req, res) => {
+    const result = await prisma.rule.create({
+        data: {
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            name: req.body?.name,
+            rawy: req.body?.rawy,
+            matn: req.body?.matn,
+        }
+    })
+    console.log(result)
+    res.send(result)
+})
+
+app.put('/api/:id', async (req, res) => {
+    
+    // create the body of a put request that updates a rule in the prisma database
+
+    const updated = await prisma.rule.update({
+        where: { id: req.params?.id },
+        data: {
+            name: req.body?.name,
+            rawy: req.body?.rawy,
+            matn: req.body?.matn,
+        }
+    })
+    res.send(updated)
 })
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));    
--- backend/lib/prisma.js
+++ backend/lib/prisma.js
@@ -1,3 +1,4 @@
-const prisma = require('@prisma/client');
+import { PrismaClient } from '@prisma/client';
 
-module.exports = new prisma.PrismaClient();;
+const prisma = new PrismaClient();
+export default prisma;
